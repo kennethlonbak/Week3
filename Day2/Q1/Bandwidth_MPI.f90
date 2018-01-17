a3fd02e1c129e747a94d0fbec0713008d2386c8e
Kenneth Loenbaek <kennethlonbak@hotmail.com>
2018-01-17 13:39:53 +0100

    Day2 Q1: Changed Bandwidth to give the bandwidth and lantency

--- Day2/Q1/Bandwidth_MPI.f90
+++ Day2/Q1/Bandwidth_MPI.f90
@@ -3,51 +3,106 @@ PROGRAM Bandwidth_MPI
     INCLUDE "mpif.h"
     ! Declaring
     INTEGER, PARAMETER ::  MK=KIND(1d0)
-    INTEGER(MK), PARAMETER :: msg_len = 200, n=2**18
-    INTEGER(MK) :: i
+    INTEGER(MK), PARAMETER :: n_array = 2**18
+    INTEGER(MK) :: i, k, imax_num = 1e6, imax_array = 1e4
     INTEGER :: rank, Npr, ierror
     INTEGER :: status(MPI_STATUS_SIZE)
-    REAL(MK), DIMENSION(n) :: array_send, array_recv
-
-    ! Initilizing
+    CHARACTER(LEN=100) :: fileout
+    REAL(MK), DIMENSION(n_array) :: array_send=1d0, array_recv
+    REAL(8) :: ts, te
+    INTEGER :: num_send, num_recv
 
     CALL MPI_Init(ierror)
     CALL MPI_Comm_Rank(MPI_COMM_WORLD,rank,ierror)
     CALL MPI_Comm_Size(MPI_COMM_WORLD,Npr,ierror)
-    WRITE(*,*) "Rank:", rank, " (Npr:", Npr, ")"
 
-    DO i=1,n
-        IF (rank == 0) THEN
-            ! Send
-            array_send(i) = MPI_WTIME()
-            CALL MPI_SSend(array_send(1:i),i,MPI_DOUBLE_PRECISION,1,i,MPI_COMM_WORLD,ierror)
-            ! Recive
-            CALL MPI_Recv(array_recv(1:i),i,MPI_DOUBLE_PRECISION,1,i-1,MPI_COMM_WORLD,status,ierror)
-            array_send(i) = MPI_WTIME()-array_send(i)
-            !WRITE(*,*) "(Rank 0) Last element recv:", array_recv(i)
-        ELSE
-            ! Recive
-            CALL MPI_Recv(array_recv(1:i),i,MPI_DOUBLE_PRECISION,0,i,MPI_COMM_WORLD,status,ierror)
-            !WRITE(*,*) "(Rank 1) Last element recv:", array_recv(i)
-            ! Send
-            CALL MPI_SSend(array_send(1:i),i,MPI_DOUBLE_PRECISION,0,i-1,MPI_COMM_WORLD,ierror)
-        END IF
-    END DO
-    CALL MPI_Barrier(MPI_COMM_WORLD,ierror)
-
-    IF (rank == 0) THEN
-        OPEN(61,FILE="TIMING.dat",action="write")
-        DO i = 1,n
-            WRITE(61,*) array_send(i), i*8
-        END DO
+    IF (MOD(Npr,2) /= 0) THEN
+        WRITE(*,*) "Npr needs to be an even number! (Given Npr=",Npr,")"
+        CALL MPI_Finalize(ierror)
+        STOP
     END IF
 
+    ! ------------------------ Latency -------------------------------- !
+    ts  = MPI_WTIME()
+    DO i=1,imax_num
+        IF (MOD(rank,2) == 0) THEN
+            ! Send
+            !WRITE(*,*) "SEND", rank+1,rank
+            CALL MPI_SSend(num_send,1,MPI_INTEGER,rank+1,rank,MPI_COMM_WORLD,ierror)
+            ! Recive
+            CALL MPI_Recv(num_recv,1,MPI_INTEGER,rank+1,rank+1,MPI_COMM_WORLD,status,ierror)
+        ELSE
+            ! Recive
+            !WRITE(*,*) "RECV", rank-1,rank-1
+            CALL MPI_Recv(num_recv,1,MPI_INTEGER,rank-1,rank-1,MPI_COMM_WORLD,status,ierror)
+            ! Send
+            CALL MPI_SSend(num_send,1,MPI_INTEGER,rank-1,rank,MPI_COMM_WORLD,ierror)
+        END IF
+    END DO
+    te = MPI_WTIME()
+    IF (MOD(rank,2) == 0) THEN
+        WRITE(*,"(A,E10.4,A,I1,AI1,A,F15.7,A)") "Avg. Latency is: ", (te-ts)/imax_num, &
+                " s (Rank ", rank, "<->Rank ",rank+1,", t_tot:", te-ts, ")"
+    END IF
+
+    ! ------------------- Bandwidth -------------------------------------- !
+    ts  = MPI_WTIME()
+    DO i=1,imax_array
+        IF (MOD(rank,2) == 0) THEN
+            ! Send
+            CALL MPI_SSend(array_send,n_array,MPI_DOUBLE_PRECISION,rank+1,rank,MPI_COMM_WORLD,ierror)
+            ! Recive
+            CALL MPI_Recv(array_recv,n_array,MPI_DOUBLE_PRECISION,rank+1,rank+1,MPI_COMM_WORLD,status,ierror)
+        ELSE
+            ! Recive
+            CALL MPI_Recv(array_recv,n_array,MPI_DOUBLE_PRECISION,rank-1,rank-1,MPI_COMM_WORLD,status,ierror)
+            ! Send
+            CALL MPI_SSend(array_send,n_array,MPI_DOUBLE_PRECISION,rank-1,rank,MPI_COMM_WORLD,ierror)
+        END IF
+    END DO
+    te = MPI_WTIME()
+    IF (MOD(rank,2) == 0) THEN
+        WRITE(*,"(A,f8.1,A,I1,AI1,A,F15.7,A)") "Avg. Bandwidth is: ", real(8*n_array*imax_array)/(1d6*(te-ts)), &
+                " MB/s (Rank ", rank, "<->Rank ",rank+1,", t_tot:", te-ts, ")"
+    END IF
+
+
     CALL MPI_Finalize(ierror)
-    CALL SLEEP(1)
-
-
 
 
 END PROGRAM Bandwidth_MPI
+
+! Running:
+!
+! mpirun -np 2 Bandwidth_MPI.prog
+! Avg. Latency is: 0.1061E-05 s (Rank 0<->Rank 1, t_tot:      1.0606183)
+! Avg. Bandwidth is:   7326.6 MB/s (Rank 0<->Rank 1, t_tot:      2.8623627)
+!
+! mpirun -np 4 Bandwidth_MPI.prog
+! Avg. Latency is: 0.1082E-05 s (Rank 2<->Rank 3, t_tot:      1.0818536)
+! Avg. Latency is: 0.1092E-05 s (Rank 0<->Rank 1, t_tot:      1.0919312)
+! Avg. Bandwidth is:   4733.0 MB/s (Rank 0<->Rank 1, t_tot:      4.4308716)
+! Avg. Bandwidth is:   4702.3 MB/s (Rank 2<->Rank 3, t_tot:      4.4598312)
+!
+! mpirun -np 6 Bandwidth_MPI.prog
+! Avg. Latency is: 0.1293E-05 s (Rank 0<->Rank 1, t_tot:      1.2932095)
+! Avg. Latency is: 0.1333E-05 s (Rank 2<->Rank 3, t_tot:      1.3325731)
+! Avg. Latency is: 0.1333E-05 s (Rank 4<->Rank 5, t_tot:      1.3330252)
+! Avg. Bandwidth is:   2980.2 MB/s (Rank 0<->Rank 1, t_tot:      7.0368865)
+! Avg. Bandwidth is:   2885.8 MB/s (Rank 2<->Rank 3, t_tot:      7.2672328)
+! Avg. Bandwidth is:   2875.8 MB/s (Rank 4<->Rank 5, t_tot:      7.2924287)
+!
+! mpirun -np 8 Bandwidth_MPI.prog
+! Avg. Latency is: 0.1512E-05 s (Rank 2<->Rank 3, t_tot:      1.5117851)
+! Avg. Latency is: 0.1552E-05 s (Rank 6<->Rank 7, t_tot:      1.5515365)
+! Avg. Latency is: 0.1573E-05 s (Rank 0<->Rank 1, t_tot:      1.5732317)
+! Avg. Latency is: 0.1716E-05 s (Rank 4<->Rank 5, t_tot:      1.7155836)
+! Avg. Bandwidth is:   2188.3 MB/s (Rank 0<->Rank 1, t_tot:      9.5833186)
+! Avg. Bandwidth is:   2169.1 MB/s (Rank 6<->Rank 7, t_tot:      9.6681903)
+! Avg. Bandwidth is:   2101.7 MB/s (Rank 4<->Rank 5, t_tot:      9.9785839)
+! Avg. Bandwidth is:   2057.2 MB/s (Rank 2<->Rank 3, t_tot:     10.1939995)
+
+
+
 
 
--- Day2/Q1/Timing.py
+++ Day2/Q1/Timing.py
@@ -1,9 +1,26 @@
 import pylab as py
-
-data = py.loadtxt("TIMING.dat")
-
+data = {}
+n_run = 2
+n_rank = 2
+for run in range(1,n_run):
+    data[run] = {}
+    for rank in range(0,n_rank,2):
+        print("Loading rank:%d for run:%d"%(rank,run))
+        data[run][rank] = py.loadtxt("TIMING_rank%d_run%02d.dat"%(rank,run))
+nr = 1
+#mean = py.zeros(shape=py.shape(data[0][0][nr:,0]))
+n=0
 fig, ax = py.subplots(1,1)
-ax.plot(data[:,1]/(1e6),data[:,0])
-#ax.set_xlim(0,0.0003)
+for run in range(1,n_run):
+    for rank in range(0,n_rank,2):
+        n += 1
+        print("Plotting rank:%d for run:%d" % (rank, run))
+        t_rmean = py.convolve(data[run][rank][:,0],py.ones([nr,1]))
+        b_rmean = py.convolve(data[run][rank][:,1]/1e6,py.ones([nr,1]))
+        ax.plot(b_rmean,t_rmean,label="rank%d run%d"%(rank,run))
+        #mean += t_rmean
 
+#ax.plot(b_rmean,mean/n,label='Mean')
+#ax.set_xlim(0,0.0003)
+ax.legend()
 py.show()
